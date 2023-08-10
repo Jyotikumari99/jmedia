71045968b6fce2cb9e0e22864b066b0d04f3bd88
Jyotikumari99 <chunniyadav99@gmail.com>
2023-08-10 18:08:22 +0530

    added chatbot

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 const video = document.getElementById("video");
 
 function getCurrentURL() {
-    return window.location.href;
+  return window.location.href;
 }
 const url = getCurrentURL();
 const pos = url.search("/ailabs.html");
 function startVideo() {
-    navigator.mediaDevices
-        .getUserMedia({ video: {} })
-        .then((stream) => {
-            video.srcObject = stream;
-        })
-        .catch((err) => {
-            console.error(err);
-        });
+  navigator.mediaDevices
+    .getUserMedia({ video: {} })
+    .then((stream) => {
+      video.srcObject = stream;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
 if (pos !== -1) {
-    Promise.all([
-        faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
-        faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
-        faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
-        faceapi.nets.faceExpressionNet.loadFromUri("./models"),
-    ]).then(startVideo);
+  Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
+    faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
+    faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+    faceapi.nets.faceExpressionNet.loadFromUri("./models"),
+  ]).then(startVideo);
 }
 video.addEventListener("play", () => {
-    setInterval(async () => {
-        const detection = await faceapi.detectAllFaces(video, new faceapi());
-    }, 100);
+  setInterval(async () => {
+    const detection = await faceapi.detectAllFaces(video, new faceapi());
+  }, 100);
 });
